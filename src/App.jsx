027b98fc9a046113dcfd283f9af3f1ec8b2789ff
iftekhar mahmud <iftekharmahmud98@gmail.com>
2023-09-12 18:35:48 +0600

    added blog section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './Componants/Header/Header'
 import Blogs from './Componants/Blogs/Blogs'
 
 import './App.css'
+import BookMarks from './Componants/BookMarks/BookMarks'
 
 function App() {
 
@@ -9,7 +10,10 @@ function App() {
   return (
     <>
        <Header></Header>
-       <Blogs></Blogs>
+        <div className='md:flex'>
+        <Blogs></Blogs>
+        <BookMarks></BookMarks>
+        </div>
 
     </>
   )
--- src/Componants/Header/Header.jsx
+++ src/Componants/Header/Header.jsx
@@ -1,9 +1,10 @@
-
+import Profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <div>
-             <h1 className='text-2xl bg-pink-400 font-bold'>Knowledge Cafe</h1>
-        </div>
+        <header className='flex justify-between bg-pink-500 items-center p-4 mx-4 '>
+             <h1 className='text-2xl  font-bold'>Knowledge Cafe</h1>
+             <img src={Profile} alt="" />
+        </header>
     );
 };
 
